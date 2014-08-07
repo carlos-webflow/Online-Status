84b8738b1f87b068c66bc04cf0884d711007c340
Carlos Silva <carlos@webflow.co>
2014-08-07 19:55:02 +0100

    Distinct Anonymous Users by ID

--- Online-Status/classes/class.SiteUser.lib.php
+++ Online-Status/classes/class.SiteUser.lib.php
@@ -5,27 +5,30 @@
 	class SiteUser
 	{
 		public $data = array();
+        public $dir = './onlineusers/';
 		public function __construct()
 		{
 			return ( '{' . __METHOD__ . '}' );
 		}
-		public function login($user)
+		public function login($user,$id)
 		{
 			$this->data['name'] = $user;
+            $this->data['id'] = $id;
 		}
 		public function online($user,$status)
 		{
 			$this->data['status'] = $status;
-			$fp = fopen('./onlineusers/'.$user,'wb'); fwrite($fp,$status); fclose($fp);
+            if(!is_dir($this->dir)){ mkdir($this->dir, 0777); }
+			$fp = fopen($this->dir.$user,'wb'); fwrite($fp,$status); fclose($fp);
 			$onlineNow = array();
 			$awayNow = array();
-			$usersonline = scandir('./onlineusers/');
+			$usersonline = scandir($this->dir);
 			foreach($usersonline as $useronline){
-				if(time() > filemtime('./onlineusers/'.$useronline)+300 && ($useronline!='.' || $useronline!='..')){
-					unlink('./onlineusers/'.$useronline);
+				if(time() > filemtime($this->dir.$useronline)+300 && ($useronline!='.' || $useronline!='..')){
+					unlink($this->dir.$useronline);
 				}else{
 					if($useronline!='.' || $useronline!='..'){
-						$status = file_get_contents('./onlineusers/'.$useronline);
+						$status = file_get_contents($this->dir.$useronline);
 						if($status=='online'){	array_push($onlineNow, $useronline); }
 						if($status=='away'){	array_push($awayNow, $useronline); }
 					}
--- Online-Status/online-status.php
+++ Online-Status/online-status.php
@@ -13,19 +13,19 @@ $func = new Functions;
 $user = new SiteUser;
 
 if(!isset($_REQUEST['user'])){
-	$_REQUEST['user'] = 'anonymous'.time();
+	$_REQUEST['user'] = '@nonymous';
 }
 
 if(empty($_SESSION['user'])){
-	$user->login($_REQUEST['user']);
-	$user->online();
+	$user->login($_REQUEST['user'],time());
 	$_SESSION['user'] = $user->data;
+    $user->online($_SESSION['user']['name'].'.'.$_SESSION['user']['id'],'online');
 }
 
 if(isset($_REQUEST['callback']) && ($_REQUEST['callback']=='away' || $_REQUEST['callback']=='online')){
 	header('Content-type: text/json');
 	echo($_REQUEST['callback']);?>(<?php
-	echo(json_encode($user->online($_SESSION['user']['name'], $_REQUEST['callback'])));
+	echo(json_encode($user->online($_SESSION['user']['name'].'.'.$_SESSION['user']['id'], $_REQUEST['callback'])));
 	?>);<?php
 	die();
 }
@@ -38,48 +38,55 @@ if(isset($_REQUEST['callback']) && ($_REQUEST['callback']=='away' || $_REQUEST['
 <script src="http://code.jquery.com/jquery-2.1.0.min.js" type="text/javascript"></script>
 <script src="http://code.jquery.com/ui/1.10.4/jquery-ui.js" type="text/javascript"></script>
 <script type="text/javascript">
-$().ready(function() {
-	function setStatus(status){
-		$.ajax({
-			url:document.URL,
-			type:"POST",
-			data:"",
-			contentType:"application/json; charset=utf-8",
-			dataType:"jsonp",
-			jsonpCallback:status,
-			success: function(users){
-				
-				onlineNow = '<ul>';
-				$.each(users['onlineNow'], function(index,value){
-					onlineNow += '<li>'+value+'</li>' ;
-				});
-				onlineNow += '</ul>';
-				$('#online_users_list').html(onlineNow);
-				
-				awayNow = '<ul>';
-				$.each(users['awayNow'], function(index,value){
-					awayNow += '<li>'+value+'</li>' ;
-				});
-				awayNow += '</ul>';
-				$('#away_users_list').html(awayNow);
-				
-			}
-		})
-	}
-	setInterval(setStatus,60000,'away');
-	setStatus('online');
-});
+    $().ready(function() {
+        setInterval(setStatus,60000,'away');
+        setStatus('online');
+    });
+
+    function setStatus(status){
+        $.ajax({
+            url:document.URL,
+            type:"POST",
+            data:"",
+            contentType:"application/json; charset=utf-8",
+            dataType:"jsonp",
+            jsonpCallback:status,
+            success: function(users){
+                console.log(users);
+                onlineNow = '<ul>';
+                $.each(users['onlineNow'], function(index,value){
+                    name = value.split('.')[0];
+                    id = value.split('.')[1];
+                    onlineNow += '<li title="'+id+'">'+name+'</li>' ;
+                });
+                onlineNow += '</ul>';
+                $('#online_users_list').html(onlineNow);
+
+                awayNow = '<ul>';
+                $.each(users['awayNow'], function(index,value){
+                    name = value.split('.')[0];
+                    id = value.split('.')[1];
+                    awayNow += '<li title="'+id+'">'+name+'</li>' ;
+                });
+                awayNow += '</ul>';
+                $('#away_users_list').html(awayNow);
+
+            }
+        })
+        return "Status is now ["+status+"]"
+    }
+
 </script>
 </head>
 <body>
-<div id="online_users" style="color:#00AD7A; font-family:'Gill Sans', 'Gill Sans MT', 'Myriad Pro', 'DejaVu Sans Condensed', Helvetica, Arial, sans-serif;">
+<div id="online_users" style="color:#00AD7A; font-family:Helvetica, Arial, sans-serif;">
 <span style="font-weight:bolder;">[Online Users]</span>
-<span id="online_users_list"></span>
+<span id="online_users_list">...</span>
 </div>
 <br>
-<div id="away_users" style="color:#C8B400; font-family:'Gill Sans', 'Gill Sans MT', 'Myriad Pro', 'DejaVu Sans Condensed', Helvetica, Arial, sans-serif;">
+<div id="away_users" style="color:#C8B400; font-family:Helvetica, Arial, sans-serif;">
 <span style="font-weight:bolder;">[Away Users]</span>
-<span id="away_users_list"></span>
+<span id="away_users_list">...</span>
 </div>
 </body>
 </html>
